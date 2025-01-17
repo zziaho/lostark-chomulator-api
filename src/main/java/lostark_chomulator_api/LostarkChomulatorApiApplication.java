f15b4d735ede2421ace193a9a9fc1b6f5ce1f4d3
zziaho <rlawlgh1100@naver.com>
2025-01-17 19:14:35 +0900

    feat: init

---
+++ src/main/java/lostark_chomulator_api/LostarkChomulatorApiApplication.java
@@ -0,0 +1,13 @@
+package lostark_chomulator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkChomulatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostarkChomulatorApiApplication.class, args);
+	}
+
+}
